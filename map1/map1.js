d2742f9c77f46d3580d8ca03baf95d33e1745dc2
User1 <user@example.com>
2020-04-15 19:47:53 -0500

    For Lab 7

---
+++ map1/map1.js
@@ -0,0 +1,11 @@
+var Lab7map = L.map('Lab7map').setView([38.88, -77.03], 4)
+L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}').addTo(Lab7map)
+var washingtonMonument = L.marker([38.88, -77.03]).addTo(Lab7map)
+var circle = L.polygon([
+  [38.89, -77.0365],
+  [38.88, -77.0502],
+  [38.88, -77.0326]
+]).addTo(Lab7map)
+circle.bindPopup('A view of Washington DC.')
+washingtonMonument.bindPopup('This is the Washington Monument.')
+/* global L */
