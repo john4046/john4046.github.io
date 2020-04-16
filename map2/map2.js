c068340e38bb5cdf48f81cb9cf87ce387f45e313
User1 <user@example.com>
2020-04-16 09:28:34 -0500

    Lab 8

---
+++ map2/map2.js
@@ -0,0 +1,14 @@
+/* global L */
+L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png').addTo(Map2)
+L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
+  layers: 'nexrad-n0r-900913',
+  format: 'image/png',
+  transparent: true,
+  attribution: 'NOAA, Iowa State University'
+}).addTo(Map2)
+L.tileLayer.wms(https://mesonet.agron.iastate.edu/cgi-bin/wms/us/wwa.cgi, {
+  layers: 'warnings_c',
+  format: 'image/png',
+  transparent: true,
+  attribution: 'NOAA, Iowa State University'
+}).addTo(Map2)
