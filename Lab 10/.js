cdf1df7aba6801e04d4538596731499aae0bec4d
john4046 <63376537+john4046@users.noreply.github.com>
2020-04-27 15:57:07 -0500

    Update .js

--- Lab 10/.js
+++ Lab 10/.js
@@ -1,3 +1,15 @@
 /* global L */
 var Lab10map = L.map('Lab10map').setView([38.88, -77.03], 4)
 L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png').addTo(Lab10map)
+<h1>Whereismymap?<h1>
+<h4>Look at this map!</h4>
+<p>Paragraph</p>
+<a href="index.html">
+<a href="https://john4046.github.io">
+<img src="https://www.freepnglogos.com/uploads/mountain-png/matterhorn-snow-mountain-photo-pixabay-12.png" alt="Mountain">
+<img src="https://john4046.github.io/icon.png" alt="My Image">
+<hr>Horizontal rule (line)
+<p>Default paragraph text</p>
+<p class="text-info">Paragraph text with a Bootstrap class</p>
+<p>Default text with <span class="text-info">formatted text</span> mixed in</p>
+<div id="Lab10map"
